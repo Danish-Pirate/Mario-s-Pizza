79a95533576dd47f4e9303ef1c3baec49ca44658
Danish-Pirate <arja0015@stud.kea.dk>
2021-10-14 10:09:25 +0200

    Did stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MenuCreate menu = new MenuCreate();
+        System.out.println(menu);
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 public class Pizza {
   int pizzaNummer;
@@ -13,21 +12,23 @@ public class Pizza {
     this.pizzaPris = pizzaPris;
   }
 
-  public void createMenu() {
-    ArrayList<Pizza> pizzaList = new ArrayList<>();
-    pizzaList.add(new Pizza(1, "Vesuvio", "tomatsauce, ost, skinke og oregano", 57));
-    pizzaList.add(new Pizza(2, "Amerikaner", "tomatsauce, ost, oksefars og oregano", 53));
-    pizzaList.add(new Pizza(3, "Cacciatore", "tomatsauce, ost, pepperoni og oregano", 57));
-    pizzaList.add(new Pizza(4, "Carbona", "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63));
-    pizzaList.add(new Pizza(5, "Dennis", "tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65));
-    pizzaList.add(new Pizza(6, "Bertil", "tomatsauce, ost, bacon og oregano", 57));
-    pizzaList.add(new Pizza(7, "Silvia", "tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano", 61));
-    pizzaList.add(new Pizza(8, "Victoria", "tomatsauce, ost, skinke, ananas, champignon og oregano", 61));
-    pizzaList.add(new Pizza(9, "Toronfo", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano", 61));
-    pizzaList.add(new Pizza(10, "Capricciosa", "tomatsauce, ost, skinke, champignon og oregano", 61));
-    pizzaList.add(new Pizza(11, "Hawaii", "tomatsauce, ost, skinke, ananas og oregano", 61));
-    pizzaList.add(new Pizza(12, "Le Blissola", "tomatsauce, ost, skinke, rejer og oregano", 61));
-    pizzaList.add(new Pizza(13, "Venezia", "tomatsauce, ost, skinke, bacon og oregano", 61));
-    pizzaList.add(new Pizza(14, "Mafia", "tomatsauce, ost, pepperoni, bacon og oregano", 61));
+  public int getPizzaNummer() {
+    return pizzaNummer;
+  }
+
+  public String getPizzaNavn() {
+    return pizzaNavn;
+  }
+
+  public String getPizzaIngredienser() {
+    return pizzaIngredienser;
+  }
+
+  public int getPizzaPris() {
+    return pizzaPris;
+  }
+
+  public String toString() {
+    return pizzaNummer +": " + pizzaNavn;
   }
 }
